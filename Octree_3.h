4b0d9cc517a8d44ad5487097d6c1ef7b0d812777
Jackson Campolattaro <jackcamp@vt.edu>
2020-05-20 23:47:44 -0400

    Add iostream for self sufficiency.

--- Octree_3.h
+++ Octree_3.h
@@ -38,11 +38,12 @@
 #include <CGAL/Search_traits_adapter.h>
 
 /*
- * These boost headers were not included here originally
+ * These headers were not included here originally
  * Adding them was necessary to make this header self sufficient
  */
 #include <boost/function.hpp>
 #include <boost/bind.hpp>
+#include <iostream>
 
 #include <stack>
 #include <queue>
