6623d57e4b4fb7f9d4d261b55bc52e2f78941e8e
Jackson Campolattaro <jackcamp@vt.edu>
2020-05-21 13:26:09 -0400

    Import Cartesian kernel and test printing a point

--- benchmark.cpp
+++ benchmark.cpp
@@ -4,8 +4,17 @@
 #include "Octree.h"
 #include "Octree_3.h"
 
+#include <CGAL/Cartesian.h>
+
 #include <catch2/catch.hpp>
+
+
+typedef CGAL::Cartesian<float>  Kernel;
+typedef Kernel::Point_3 Point_3;
 
 TEST_CASE("Benchmark") {
 
+    Point_3 p{1.5, 2.0, 2.5};
+
+    std::cout << p << std::endl;
 }
