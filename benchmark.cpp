fc6ff354972eee24ddd88ea4d5c04a6320fd7ed3
Jackson Campolattaro <jackcamp@vt.edu>
2020-06-02 20:18:53 -0400

    Split runs into their own function, introduce use of CATCH benchmarks

--- benchmark.cpp
+++ benchmark.cpp
@@ -20,26 +20,54 @@
 #include <istream>
 
 // Used for benchmarking
+#define CATCH_CONFIG_ENABLE_BENCHMARKING
+
 #include <catch2/catch.hpp>
 
-typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
-typedef Kernel::FT FT;
-typedef Kernel::Point_3 Point;
-typedef CGAL::Point_set_3<Point> Point_set;
+template<class Kernel, class Point>
+void bench(CGAL::Point_set_3<Point> points) {
 
-typedef CGAL::Shape_detection::Efficient_RANSAC_traits
-        <Kernel, Point_set, Point_set::Point_map, Point_set::Vector_map>
-        Traits;
-typedef CGAL::Shape_detection::internal::Octree
-        <CGAL::Shape_detection::internal::DirectPointAccessor<Traits>>
-        OldOctree;
+    // Defining shared types
+    typedef CGAL::Point_set_3<Point> Point_set;
 
-typedef CGAL::OCTREE::Octree
-        <Kernel, Point_set::Point_range, Point_set::Point_map, Point_set::Vector_map>
-        NewOctree;
+    // Defining the Old Octree
+    typedef CGAL::Shape_detection::Efficient_RANSAC_traits
+            <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
+            Traits;
+    typedef CGAL::Shape_detection::internal::Octree
+            <CGAL::Shape_detection::internal::DirectPointAccessor<Traits>>
+            OldOctree;
+
+    // Defining the New Octree
+    typedef CGAL::OCTREE::Octree
+            <Kernel, Point_set, typename Point_set::Point_map, typename Point_set::Vector_map>
+            NewOctree;
 
 
-TEST_CASE("Random points in cubic volume") {
+    // Benchmark
+    // ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+    // Build the old octree
+    auto point_map = points.point_map();
+    auto normal_map = points.normal_map();
+    auto input_iterator_first = points.begin();
+    auto input_iterator_beyond = points.end();
+
+    OldOctree oldOctree((Traits()), input_iterator_first, input_iterator_beyond, point_map, normal_map);
+
+    // Build the new octree
+    // FIXME: I'm having trouble constructing the new octree
+    //NewOctree newOctree(points, point_map, normal_map);
+
+    // TODO
+}
+
+
+TEST_CASE("Random exact points in a cubic volume") {
+
+    typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+    typedef Kernel::Point_3 Point;
+    typedef CGAL::Point_set_3<Point> Point_set;
 
     std::size_t N = 1000;
 
@@ -47,23 +75,17 @@ TEST_CASE("Random points in cubic volume") {
     Point_set points;
     CGAL::Random_points_in_cube_3<Point> generator;
     points.reserve(N);
-    for (std::size_t i = 0; i < N; ++ i)
-        points.insert (*(generator ++));
+    for (std::size_t i = 0; i < N; ++i)
+        points.insert(*(generator++));
 
-    // Build the old octree
-    Traits traits;
-    OldOctree oldOctree(traits);
-    //oldOctree.createTree();
-    // TODO
-
-    // Build the new octree
-    // FIXME: I'm having trouble constructing the new octree
-    //NewOctree newOctree(points.points(), points.point_map(), points.normal_map());
-
-    // TODO
+    BENCHMARK("Random exact points in a cubic volume") {return bench<Kernel, Point>(points);};
 }
 
-TEST_CASE("Random points on spherical surface") {
+TEST_CASE("Random exact points on spherical surface") {
+
+    typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+    typedef Kernel::Point_3 Point;
+    typedef CGAL::Point_set_3<Point> Point_set;
 
     std::size_t N = 1000;
 
@@ -71,55 +93,36 @@ TEST_CASE("Random points on spherical surface") {
     Point_set points;
     CGAL::Random_points_on_sphere_3<Point> generator;
     points.reserve(N);
-    for (std::size_t i = 0; i < N; ++ i)
-        points.insert (*(generator ++));
+    for (std::size_t i = 0; i < N; ++i)
+        points.insert(*(generator++));
 
-    // Build the old octree
-    Traits traits;
-    OldOctree oldOctree(traits);
-    // TODO
-
-    // Build the new octree
-    // FIXME: I'm having trouble constructing the new octree
-    //NewOctree newOctree(points.points(), points.point_map(), points.normal_map());
-
-    // TODO
+    BENCHMARK("Random exact points on spherical surface") {return bench<Kernel, Point>(points);};
 }
 
-TEST_CASE("Cleaned Statue surface") {
+TEST_CASE("Cleaned Statue surface with exact points") {
+
+    typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+    typedef Kernel::Point_3 Point;
+    typedef CGAL::Point_set_3<Point> Point_set;
 
     // Read example data from file
     std::ifstream stream("../data/archer_cleaned.ply");
     Point_set points;
     stream >> points;
 
-    // Build the old octree
-    Traits traits;
-    OldOctree oldOctree(traits);
-    // TODO
-
-    // Build the new octree
-    // FIXME: I'm having trouble constructing the new octree
-    //NewOctree newOctree(points.points(), points.point_map(), points.normal_map());
-
-    // TODO
+    BENCHMARK("Cleaned Statue surface with exact points") {return bench<Kernel, Point>(points);};
 }
 
-TEST_CASE("Uncleaned Statue surface") {
+TEST_CASE("Uncleaned Statue surface with exact points") {
+
+    typedef CGAL::Exact_predicates_inexact_constructions_kernel Kernel;
+    typedef Kernel::Point_3 Point;
+    typedef CGAL::Point_set_3<Point> Point_set;
 
     // Read example data from file
     std::ifstream stream("../data/archer_original.ply");
     Point_set points;
     stream >> points;
 
-    // Build the old octree
-    Traits traits;
-    OldOctree oldOctree(traits);
-    // TODO
-
-    // Build the new octree
-    // FIXME: I'm having trouble constructing the new octree
-    //NewOctree newOctree(points.points(), points.point_map(), points.normal_map());
-
-    // TODO
+    BENCHMARK("Uncleaned Statue surface with exact points") {return bench<Kernel, Point>(points);};
 }
